b59193a492298219c9b80706dbb87a23aec26654
pwliuab“ <q374536937@gmail.com>
2021-12-19 10:52:17 +0800

    comp4021 resources

---
+++ comp4021_lab/Lab5/lab5-solution/game-5-solution/game.js
@@ -0,0 +1,358 @@
+// The point and size class used in this program
+function Point(x, y) {
+    this.x = (x)? parseFloat(x) : 0.0;
+    this.y = (y)? parseFloat(y) : 0.0;
+}
+
+function Size(w, h) {
+    this.w = (w)? parseFloat(w) : 0.0;
+    this.h = (h)? parseFloat(h) : 0.0;
+}
+
+// Helper function for checking intersection between two rectangles
+function intersect(pos1, size1, pos2, size2) {
+    return (pos1.x < pos2.x + size2.w && pos1.x + size1.w > pos2.x &&
+            pos1.y < pos2.y + size2.h && pos1.y + size1.h > pos2.y);
+}
+
+
+// The player class used in this program
+function Player() {
+    this.node = document.getElementById("player");
+    this.position = PLAYER_INIT_POS;
+    this.motion = motionType.NONE;
+    this.verticalSpeed = 0;
+    this.facingDir = faceDirection.RIGHT;
+}
+
+Player.prototype.isOnPlatform = function() {
+    var platforms = document.getElementById("platforms");
+    for (var i = 0; i < platforms.childNodes.length; i++) {
+        var node = platforms.childNodes.item(i);
+        if (node.nodeName != "rect") continue;
+
+        var x = parseFloat(node.getAttribute("x"));
+        var y = parseFloat(node.getAttribute("y"));
+        var w = parseFloat(node.getAttribute("width"));
+        var h = parseFloat(node.getAttribute("height"));
+
+        if (((this.position.x + PLAYER_SIZE.w > x && this.position.x < x + w) ||
+             ((this.position.x + PLAYER_SIZE.w) == x && this.motion == motionType.RIGHT) ||
+             (this.position.x == (x + w) && this.motion == motionType.LEFT)) &&
+            this.position.y + PLAYER_SIZE.h == y) return true;
+    }
+    if (this.position.y + PLAYER_SIZE.h == SCREEN_SIZE.h) return true;
+
+    return false;
+}
+
+Player.prototype.collidePlatform = function(position) {
+    var platforms = document.getElementById("platforms");
+    for (var i = 0; i < platforms.childNodes.length; i++) {
+        var node = platforms.childNodes.item(i);
+        if (node.nodeName != "rect") continue;
+
+        var x = parseFloat(node.getAttribute("x"));
+        var y = parseFloat(node.getAttribute("y"));
+        var w = parseFloat(node.getAttribute("width"));
+        var h = parseFloat(node.getAttribute("height"));
+        var pos = new Point(x, y);
+        var size = new Size(w, h);
+
+        if (intersect(position, PLAYER_SIZE, pos, size)) {
+            position.x = this.position.x;
+            if (intersect(position, PLAYER_SIZE, pos, size)) {
+                if (this.position.y >= y + h)
+                    position.y = y + h;
+                else
+                    position.y = y - PLAYER_SIZE.h;
+                this.verticalSpeed = 0;
+            }
+        }
+    }
+}
+
+Player.prototype.collideScreen = function(position) {
+    if (position.x < 0) position.x = 0;
+    if (position.x + PLAYER_SIZE.w > SCREEN_SIZE.w) position.x = SCREEN_SIZE.w - PLAYER_SIZE.w;
+    if (position.y < 0) {
+        position.y = 0;
+        this.verticalSpeed = 0;
+    }
+    if (position.y + PLAYER_SIZE.h > SCREEN_SIZE.h) {
+        position.y = SCREEN_SIZE.h - PLAYER_SIZE.h;
+        this.verticalSpeed = 0;
+    }
+}
+
+
+//
+// Below are constants used in the game
+//
+var PLAYER_SIZE = new Size(40, 40);         // The size of the player
+var SCREEN_SIZE = new Size(600, 560);       // The size of the game screen
+var PLAYER_INIT_POS  = new Point(0, 0);     // The initial position of the player
+
+var MOVE_DISPLACEMENT = 5;                  // The speed of the player in motion
+var JUMP_SPEED = 15;                        // The speed of the player jumping
+var VERTICAL_DISPLACEMENT = 1;              // The displacement of vertical speed
+
+var GAME_INTERVAL = 25;                     // The time interval of running the game
+
+
+//
+// Variables in the game
+//
+var motionType = {NONE:0, LEFT:1, RIGHT:2}; // Motion enum
+var faceDirection = {LEFT: 0, RIGHT: 1};
+var player = null;                          // The player object
+var gameInterval = null;                    // The interval
+var zoom = 1.0;                             // The zoom level of the screen
+
+var BULLET_SIZE = new Size(10, 10); // The size of a bullet
+var BULLET_SPEED = 10.0;            // The speed of a bullet
+                                    //  = pixels it moves each game loop
+var SHOOT_INTERVAL = 200.0;         // The period when shooting is disabled
+var canShoot = true;                // A flag indicating whether the player can shoot a bullet
+
+var MONSTER_SIZE = new Size(40, 40); // The size of a monster
+
+
+// Should be executed after the page is loaded
+function load() {
+    // Attach keyboard events
+    document.addEventListener("keydown", keydown, false);
+    document.addEventListener("keyup", keyup, false);
+
+    // Create the player
+    player = new Player();
+
+    // Create the monsters
+    createMonster(200, 15);
+    createMonster(400, 270);
+
+    // Start the game interval
+    gameInterval = setInterval("gamePlay()", GAME_INTERVAL);
+}
+
+
+//
+// This function creates the monsters in the game
+//
+function createMonster(x, y) {
+    var monster = document.createElementNS("http://www.w3.org/2000/svg", "use");
+    monster.setAttribute("x", x);
+    monster.setAttribute("y", y);
+    monster.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#monster");
+    document.getElementById("monsters").appendChild(monster);
+}
+
+
+
+//
+// This function shoots a bullet from the player
+//
+function shootBullet() {
+    // Disable shooting for a short period of time
+    canShoot = false;
+    setTimeout("canShoot = true", SHOOT_INTERVAL);
+    // Create the bullet using the use node
+    var bullet = document.createElementNS("http://www.w3.org/2000/svg", "use");
+    console.log(player.facingDir);
+    console.log(faceDirection.RIGHT);
+    if(player.facingDir == faceDirection.RIGHT){
+      bullet.setAttribute("x", player.position.x + PLAYER_SIZE.w / 2 - BULLET_SIZE.w / 2);
+      bullet.setAttribute("id", "right");
+    } else {
+      bullet.setAttribute("x", player.position.x - PLAYER_SIZE.w / 2 + BULLET_SIZE.w / 2);
+      bullet.setAttribute("id", "left");
+    }
+
+    bullet.setAttribute("y", player.position.y + PLAYER_SIZE.h / 2 - BULLET_SIZE.h / 2);
+    bullet.setAttributeNS("http://www.w3.org/1999/xlink", "xlink:href", "#bullet");
+    document.getElementById("bullets").appendChild(bullet);
+}
+
+
+//
+// This function updates the position of the bullets
+//
+function moveBullets() {
+    // Go through all bullets
+    var bullets = document.getElementById("bullets");
+    for (var i = 0; i < bullets.childNodes.length; i++) {
+        var node = bullets.childNodes.item(i);
+
+        // Update the position of the bullet
+        var x = parseInt(node.getAttribute("x"));
+        if (x > SCREEN_SIZE.w || x < 0) {
+            bullets.removeChild(node);
+            i--;
+        } else {
+            if(node.getAttribute('id') == "right"){
+              node.setAttribute("x", x + BULLET_SPEED);
+            } else if(node.getAttribute('id') == 'left') {
+              node.setAttribute("x", x - BULLET_SPEED);
+            }
+          }
+        // If the bullet is not inside the screen delete it from the group
+
+    }
+}
+
+//
+// This is the keydown handling function for the SVG document
+//
+function keydown(evt) {
+    var keyCode = (evt.keyCode)? evt.keyCode : evt.getKeyCode();
+
+    switch (keyCode) {
+        case "N".charCodeAt(0):
+            player.motion = motionType.LEFT;
+            player.facingDir = faceDirection.LEFT;
+            break;
+
+        case "M".charCodeAt(0):
+            player.motion = motionType.RIGHT;
+            player.facingDir = faceDirection.RIGHT;
+            break;
+
+
+        // Add your code here
+
+
+        case "Z".charCodeAt(0):
+            if (player.isOnPlatform()) {
+                player.verticalSpeed = JUMP_SPEED;
+            }
+            break;
+
+		case 32: // spacebar = shoot
+			if (canShoot) shootBullet();
+			break;
+    }
+}
+
+
+//
+// This is the keyup handling function for the SVG document
+//
+function keyup(evt) {
+    // Get the key code
+    var keyCode = (evt.keyCode)? evt.keyCode : evt.getKeyCode();
+
+    switch (keyCode) {
+        case "N".charCodeAt(0):
+            if (player.motion == motionType.LEFT) player.motion = motionType.NONE;
+            break;
+
+        case "M".charCodeAt(0):
+            if (player.motion == motionType.RIGHT) player.motion = motionType.NONE;
+            break;
+    }
+}
+
+
+//
+// This function checks collision
+//
+function collisionDetection() {
+    // Check whether the player collides with a monster
+    var monsters = document.getElementById("monsters");
+    for (var i = 0; i < monsters.childNodes.length; i++) {
+        var monster = monsters.childNodes.item(i);
+        var x = parseInt(monster.getAttribute("x"));
+        var y = parseInt(monster.getAttribute("y"));
+
+        if (intersect(new Point(x, y), MONSTER_SIZE, player.position, PLAYER_SIZE)) {
+            alert("Game over!");
+            clearInterval(gameInterval);
+        }
+    }
+
+    // Check whether a bullet hits a monster
+    var bullets = document.getElementById("bullets");
+    for (var i = 0; i < bullets.childNodes.length; i++) {
+        var bullet = bullets.childNodes.item(i);
+        var x = parseInt(bullet.getAttribute("x"));
+        var y = parseInt(bullet.getAttribute("y"));
+
+        for (var j = 0; j < monsters.childNodes.length; j++) {
+            var monster = monsters.childNodes.item(j);
+            var mx = parseInt(monster.getAttribute("x"));
+            var my = parseInt(monster.getAttribute("y"));
+
+            if (intersect(new Point(x, y), BULLET_SIZE, new Point(mx, my), MONSTER_SIZE)) {
+                monsters.removeChild(monster);
+                j--;
+                bullets.removeChild(bullet);
+                i--;
+            }
+        }
+    }
+}
+
+//
+// This function updates the position and motion of the player in the system
+//
+function gamePlay() {
+    // Check collisions
+    collisionDetection();
+
+    // Check whether the player is on a platform
+    var isOnPlatform = player.isOnPlatform();
+
+    // Update player position
+    var displacement = new Point();
+
+    // Move left or right
+    if (player.motion == motionType.LEFT)
+        displacement.x = -MOVE_DISPLACEMENT;
+    if (player.motion == motionType.RIGHT)
+        displacement.x = MOVE_DISPLACEMENT;
+
+    // Fall
+    if (!isOnPlatform && player.verticalSpeed <= 0) {
+        displacement.y = -player.verticalSpeed;
+        player.verticalSpeed -= VERTICAL_DISPLACEMENT;
+    }
+
+    // Jump
+    if (player.verticalSpeed > 0) {
+        displacement.y = -player.verticalSpeed;
+        player.verticalSpeed -= VERTICAL_DISPLACEMENT;
+        if (player.verticalSpeed <= 0)
+            player.verticalSpeed = 0;
+    }
+
+    // Get the new position of the player
+    var position = new Point();
+    position.x = player.position.x + displacement.x;
+    position.y = player.position.y + displacement.y;
+
+    // Check collision with platforms and screen
+    player.collidePlatform(position);
+    player.collideScreen(position);
+
+    // Set the location back to the player object (before update the screen)
+    player.position = position;
+
+    moveBullets();
+    updateScreen();
+}
+
+
+//
+// This function updates the position of the player's SVG object and
+// set the appropriate translation of the game screen relative to the
+// the position of the player
+//
+function updateScreen() {
+    // Transform the player
+    player.node.setAttribute("transform", "translate(" + player.position.x + "," + player.position.y + ")");
+
+    // Calculate the scaling and translation factors
+
+    // Add your code here
+
+}
